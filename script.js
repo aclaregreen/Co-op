361779f273060e4c6fffe6e382a3dcd3f73e2316
ORS Co-op <orscoop@uoguelph.ca>
2024-06-11 09:14:40 -0400

    added menu icon

---
+++ script.js
@@ -0,0 +1,8 @@
+// function toggleMenu(){
+//     var menu = document.getElementById("")
+// }
+$(document).ready(function () {
+    $(menuButton).click(function () {
+        $("#sidebar").toggle();
+    });
+});
