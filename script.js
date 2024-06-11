d35547c281a375681173432c18535f5166fd5be4
ORS Co-op <orscoop@uoguelph.ca>
2024-06-11 09:25:29 -0400

    added jquery

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// function toggleMenu(){
-//     var menu = document.getElementById("")
-// }
 $(document).ready(function () {
     $(".menuButton").click(function () {
         $("#sidebar").toggle();
