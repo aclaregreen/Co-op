f9308a7e66e2ae6999179125fa204689e49ed12b
ORS Co-op <orscoop@uoguelph.ca>
2024-06-12 13:09:49 -0400

    menu shift js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $(".menuButton").click(function () {
         $("#sidebar").toggleClass("active");
+        $(".menuButton").toggleClass("active");
     });
 });
